528487f7f7b66e0d6f7af49a5cfa54e18bb1fef6
duyngo3225 <duyngo3225@gmail.com>
2024-10-23 23:12:32 -0500

    schedule change

--- backend/models/Pdfmodel.js
+++ backend/models/Pdfmodel.js
@@ -9,6 +9,10 @@ const PdfSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    fileHash: { // New field for content hash
+        type: String,
+        required: true,
+    },
 });
 
 const PdfModel = mongoose.model('Pdf', PdfSchema);
